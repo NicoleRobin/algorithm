c68737401422d80614ae367964fbd823db930bb2
NicoleRobin <lit050528@gmail.com>
2015-10-14 17:42:43 +0800

    complete valid number

--- Algorithm/ValidNumber/ValidNumber.cpp
+++ Algorithm/ValidNumber/ValidNumber.cpp
@@ -5,34 +5,69 @@ using namespace std;
 
 /*
 思路：
+考虑用状态机来实现，不同的类型对应不同的状态。
+但是按照自己的思路实现了之后却没有AC，因为.1按我的思路它不是数字所以返回false，但是答案却说它是true。
+我就奇怪了.1怎么就是数了？
+
+经过20次的submit终于AC了。
 */
 enum ENState
 {
-	START,		// 初始
+	INIT,		// 初始
+	START,		// 开始
 	INTEGER,	// 整型
 	FLOAT,		// 小数
-	SCIENCE,	// 科学计数法
+	SCIENCE,	// 科学
+	INDEX,		// 指数
 	END,		// 结尾
 	ERROR		// 错误
 };
 class Solution {
 public:
 	bool isNumber(string s) {
-		ENState state = START;
+		ENState state = INIT;
 		bool bIsNumeric = false;
 		for (size_t i = 0; i < s.length(); ++i)
 		{
 			switch (state)
 			{
+			case INIT:
+				if (s[i] >= 48 && s[i] <= 57)
+				{ // 整数
+					state = INTEGER;
+					bIsNumeric = true;
+				}
+				else if (s[i] == '+' || s[i] == '-')
+				{ // 开始
+					state = START;
+					bIsNumeric = false;
+				}
+				else if (s[i] == '.')
+				{ // 浮点型
+					state = FLOAT;
+					bIsNumeric = false;
+				}
+				else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+				{ // 仍是初始
+					state = INIT;
+					bIsNumeric = false;
+				}
+				else
+				{ // 错误
+					state = ERROR;
+					bIsNumeric = false;
+				}
+				break;
 			case START:
 				if (s[i] >= 48 && s[i] <= 57)
 				{
 					state = INTEGER;
 					bIsNumeric = true;
 				}
-				else if (s[i] == '+' || s[i] == '-' || s[i] == ' ')
+				else if (s[i] == '.')
 				{
-
+					state = FLOAT;
+					bIsNumeric = false;
 				}
 				else
 				{
@@ -42,12 +77,13 @@ public:
 			case INTEGER:
 				if (s[i] >= 48 && s[i] <= 57)
 				{
-
+					state = INTEGER;
+					bIsNumeric = true;
 				}
 				else if (s[i] == '.')
 				{
 					state = FLOAT;
-					bIsNumeric = false;
+					bIsNumeric = true;
 				}
 				else if (s[i] == 'e')
 				{
@@ -68,7 +104,13 @@ public:
 			case FLOAT:
 				if (s[i] >= 48 && s[i] <= 57)
 				{
+					state = FLOAT;
 					bIsNumeric = true;
+				}
+				else if (bIsNumeric && s[i] == 'e')
+				{
+					state = SCIENCE;
+					bIsNumeric = false;
 				}
 				else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
 				{
@@ -84,9 +126,32 @@ public:
 			case SCIENCE:
 				if (s[i] >= 48 && s[i] <= 57)
 				{
+					state = INDEX;
 					bIsNumeric = true;
 				}
-				else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+				else if (s[i] == '+' || s[i] == '-')
+				{
+					state = INDEX;
+					bIsNumeric = false;
+				}
+				else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\t')
+				{
+					state = END;
+					bIsNumeric = false;
+				}
+				else
+				{
+					state = ERROR;
+					bIsNumeric = false;
+				}
+				break;
+			case INDEX:
+				if (s[i] >= 48 && s[i] <= 57)
+				{
+					state = INDEX;
+					bIsNumeric = true;
+				}
+				else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\t')
 				{
 					state = END;
 				}
@@ -94,7 +159,6 @@ public:
 				{
 					state = ERROR;
 					bIsNumeric = false;
-					i = s.length();
 				}
 				break;
 			case END:
