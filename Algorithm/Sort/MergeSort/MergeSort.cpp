72ec4eb55d064b58ebf793382062c3f0e9c7b31c
nicolerobin <lit050528@gmail.com>
2017-02-14 11:13:10 +0800

    complete mergesort

--- Algorithm/Sort/MergeSort/MergeSort.cpp
+++ Algorithm/Sort/MergeSort/MergeSort.cpp
@@ -18,16 +18,49 @@ void swap(int* x, int* y)
 	*y = temp;
 }
 
-void MergeArray(int a[], int aLen, int b[], int bLen)
+void MergeArray(int a[], int aLen, int b[], int bLen, int temp[])
 {
-	if (aLen == 0 bLen == 0)
+	int i = 0, j = 0, k = 0;
+	while (i < aLen && j < bLen)
 	{
+		if (a[i] < b[j])
+		{
+			temp[k++] = a[i++];
+		}
+		else
+		{
+			temp[k++] = b[j++];
+		}
+	}
+
+	while (i < aLen)
+	{
+		temp[k++] = a[i++];
+	}
+
+	while (j < bLen)
+	{
+		temp[k++] = b[j++];
 	}
 }
 
-void MergeSort(int a[], int len)
+void MergeSort(int a[], int n)
 {
-	
+	if (n < 2)
+	{
+		return;
+	}
+
+	int m = n / 2;
+	MergeSort(a, m);
+	MergeSort(a + m, n - m);
+	int *temp = new int[n];
+	MergeArray(a, m, a + m, n - m, temp);
+	for (int i = 0; i < n; ++i)
+	{
+		a[i] = temp[i];
+	}
+	delete[] temp;
 }
 
 int main(void)
