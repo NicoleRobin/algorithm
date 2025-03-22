fd16bca17340b81d90ee1088e06499f971b40aa2
robin zhang <lit050528@gmail.com>
2025-03-23 00:53:19 +0800

    Update - add 39

--- Algorithm/go/39/39.go
+++ Algorithm/go/39/39.go
@@ -2,62 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 /*
-注意：
-1、需要判断path是否重复，例如数字个数相同，顺序不一样是被认为相同的，不能重复添加
+思路：
+1、回溯算法
+2、递归函数 backtrack(start int, path []int, sum int)
 
-目前只有一个办法是在结束时去判断
-并且，由于存储结果集是使用slice来存的，要去重就需要和已有的所有结果集进行匹配，成本有点高
+和其他回溯算法的区别：
+1、因为每个数字都可以重复使用，所以每次递归时，start 传入 i，而不是 i+1
 */
 func combinationSum(candidates []int, target int) [][]int {
-	mapResult := map[string][]int{}
-	path := []int{}
-	backtrack(candidates, path, target, mapResult)
+	var backtrack func(start int, path []int, sum int)
 
-	sliceResult := [][]int{}
-	for _, value := range mapResult {
-		sliceResult = append(sliceResult, value)
-	}
-	return sliceResult
-}
-
-func strPath(path []int) string {
-	sort.Ints(path)
-	strPath := ""
-	for _, num := range path {
-		strPath += fmt.Sprintf("%d_", num)
-	}
-	return strPath
-}
-
-func backtrack(candidates, path []int, target int, result map[string][]int) {
-	fmt.Printf("candidates:%+v, path:%+v, target:%d\n", candidates, path, target)
-	if target == 0 {
-		tmp := make([]int, len(path))
-		copy(tmp, path)
-		strPath := strPath(tmp)
-		fmt.Println(strPath)
-		if _, ok := result[strPath]; !ok {
-
-			result[strPath] = tmp
+	var result [][]int
+	backtrack = func(start int, path []int, sum int) {
+		if sum == target {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			result = append(result, temp)
 		}
-		return
-	} else if target < 0 {
-		return
+		if sum >= target {
+			return
+		}
+		for i := start; i < len(candidates); i++ {
+			path = append(path, candidates[i])
+			backtrack(i, path, sum+candidates[i])
+			path = path[:len(path)-1]
+		}
 	}
-
-	for _, candidate := range candidates {
-		fmt.Printf("candidate:%d, path:%+v\n", candidate, path)
-		path = append(path, candidate)
-		fmt.Printf("candidate:%d, path:%+v\n", candidate, path)
-		backtrack(candidates, path, target-candidate, result)
-		fmt.Printf("candidate:%d, path:%+v\n", candidate, path)
-		path = path[:len(path)-1]
-		fmt.Printf("candidate:%d, path:%+v\n", candidate, path)
-	}
+	backtrack(0, []int{}, 0)
+	return result
 }
 
 func main() {
