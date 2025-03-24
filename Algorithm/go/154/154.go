eab223f3ee5cab7adf3c1c4a5e64c4d6767e30b2
robin zhang <lit050528@gmail.com>
2025-03-24 17:58:32 +0800

    Update - fix conflict

--- Algorithm/go/154/154.go
+++ Algorithm/go/154/154.go
@@ -1,18 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func findMin(nums []int) int {
 	left, right := 0, len(nums)-1
+
 	for left < right {
+		fmt.Println("left:", left, ", right:", right)
 		mid := (left + right) / 2
 		if nums[mid] < nums[right] {
 			right = mid
-			// 为什么right不是赋值为mid-1，因为mid可能是最小值
 		} else if nums[mid] > nums[right] {
 			left = mid + 1
 		} else {
-			right--
+			if nums[left] < nums[right] {
+				break
+			} else {
+			}
 		}
 	}
 
@@ -25,9 +31,14 @@ type TestCase struct {
 
 func main() {
 	fmt.Println("leetcode 154")
+
 	testCases := []TestCase{
-		{[]int{1, 3, 5}},
-		{[]int{2, 2, 2, 0, 1}},
+		// {[]int{1, 3, 5}},
+		// {[]int{2, 2, 2, 0, 1}},
+		// {[]int{1, 1}},
+		// {[]int{1, 3, 3}},
+		{[]int{3, 3, 1, 3}},
+		// {[]int{10, 1, 10, 10, 10}},
 	}
 	for _, testCase := range testCases {
 		result := findMin(testCase.nums)
