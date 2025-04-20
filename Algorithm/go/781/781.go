aafe14ffc757a9f737af889cbe4fa5bd303aed8f
robin zhang <lit050528@gmail.com>
2025-04-20 13:58:21 +0800

    Update - add 781

--- Algorithm/go/781/781.go
+++ Algorithm/go/781/781.go
@@ -1,7 +1,24 @@
 package main
 
-func numRabbits(answers []int) int {
+/*
+思考：
+完全没有思路，好像不是常见的题型
+能想到的是hash，Key为颜色数量，Value为回答相同颜色兔子数量为Key值的个数
 
+还学到一个技巧：
+a/b的向上取整可以通过：(a + b -1)/b来实现；
+*/
+func numRabbits(answers []int) int {
+	sameCountMap := map[int]int{}
+	for _, answer := range answers {
+		sameCountMap[answer]++
+	}
+
+	var ans int
+	for answer, count := range sameCountMap {
+		ans += (answer + count) / (answer + 1) * (answer + 1)
+	}
+	return ans
 }
 
 func main() {
@@ -12,6 +29,10 @@ func main() {
 		{
 			answers:  []int{1, 1, 2},
 			expected: 5,
+		},
+		{
+			answers:  []int{10, 10, 10},
+			expected: 11,
 		},
 	}
 
