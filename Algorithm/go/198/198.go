84f88c40cd506955d220436a29a891875b7d0429
robin zhang <lit050528@gmail.com>
2025-04-22 23:37:24 +0800

    Update - add 198 2338

--- Algorithm/go/198/198.go
+++ Algorithm/go/198/198.go
@@ -1,12 +1,35 @@
 package main
 
 /*
-思路：二维动态规划
-抢了当前节点的结果：robDp[i] = max(robDp[i-2] + nums[i], notRobDp[i-1]+nums[i])
-不抢当前节点的结果：notRobDp[i] = max(robDp[i-1], notRobDp[i-1])
+思路：动态规划
+dp[i] = max(dp[i-1], dp[i-2]+nums[i])
 */
 func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
 
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+
+	return dp[len(dp)-1]
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
 }
 
 func main() {
---
+++ Algorithm/go/2338/2338.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+思路：
+1、
+*/
+func idealArrays(n int, maxValue int) int {
+	var result int
+
+	return result
+}
+
+func main() {
+
+}
