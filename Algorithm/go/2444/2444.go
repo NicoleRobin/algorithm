6fab13b91603bce6e275c6a570820c1b50502a5a
robin zhang <lit050528@gmail.com>
2025-04-29 02:02:29 +0800

    Update - add 2444

--- Algorithm/go/2444/2444.go
+++ Algorithm/go/2444/2444.go
@@ -9,7 +9,7 @@ import "math"
 3、那么剩下的问题就是：如何快速的求一个子数组的最大值、最小值；
 4、
 */
-func countSubarrays(nums []int, minK int, maxK int) int64 {
+func countSubarrays1(nums []int, minK int, maxK int) int64 {
 	var ans int64
 	for i := 0; i < len(nums); i++ {
 		for j := i; j < len(nums); j++ {
@@ -30,6 +30,32 @@ func countSubarrays(nums []int, minK int, maxK int) int64 {
 		}
 	}
 	return ans
+}
+
+/*
+官方题解
+个人理解：
+1、一次遍历数组，遇到等于minK的数，记录下标last_min，遇到等于maxK的数，记录下标last_max，遇到不在范围内的数，重置last_min和last_max为-1；
+*/
+func countSubarrays(nums []int, minK int, maxK int) int64 {
+	var res int64
+	border, last_min, last_max := -1, -1, -1
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < minK || nums[i] > maxK {
+			last_max, last_min = -1, -1
+			border = i
+		}
+		if nums[i] == minK {
+			last_min = i
+		}
+		if nums[i] == maxK {
+			last_max = i
+		}
+		if last_min != -1 && last_max != -1 {
+			res += int64(min(last_min, last_max) - border)
+		}
+	}
+	return res
 }
 
 func main() {
