fa199819919145c9b3285791548b04c7fc9717ec
robin zhang <lit050528@gmail.com>
2025-06-13 20:56:21 +0800

    Update - add 2616

--- Algorithm/go/2001_3000/2616/2616.go
+++ Algorithm/go/2001_3000/2616/2616.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
-	"runtime/pprof"
 	"sort"
 )
 
-func minimizeMax(nums []int, p int) int {
+/*
+思路：动态规划
+dp[i]表示前i个数的最小最大值
+dp[i] = min(dp[i-1], dp[i-2] + 1)
+*/
+func minimizeMax1(nums []int, p int) int {
+	dp := make([]int, len(nums)+2)
+	for i := 0; i < p; i++ {
+		dp[i+2] = max(dp[i]+1, dp[i+1])
+	}
+
+	return dp[len(nums)+1]
 }
+
+func minimizeMax(nums []int, p int) int {
+	sort.Ints(nums)
+
+	check := func(mx int) bool {
+		cnt := 0
+		for i := 0; i < len(nums)-1; i++ {
+			if nums[i+1]-nums[i] <= mx {
+				cnt++
+				i++ // 跳过下一个数
+			}
+		}
+		return cnt >= p
+	}
+
+	left, right := 0, nums[len(nums)-1]-nums[0]
+	for left < right {
+		mid := (left + right) / 2
+		if check(mid) {
+			right = mid // 尝试更小的最大值
+		} else {
+			left = mid + 1 // 增大最大值
+		}
+	}
+
+	return left
+}
+
 func main() {
 	testCases := []struct {
 		nums     []int
@@ -15,9 +53,19 @@ func main() {
 		expected int
 	}{
 		{
-			nums:     []int{},
-			p:        0,
+			nums:     []int{10, 1, 2, 7, 1, 3},
+			p:        2,
+			expected: 1,
+		},
+		{
+			nums:     []int{4, 2, 1, 2},
+			p:        1,
 			expected: 0,
+		},
+		{
+			nums:     []int{1, 1, 0, 3},
+			p:        2,
+			expected: 2,
 		},
 	}
 
