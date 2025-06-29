4698b30017be23afadb2c662f50f9af0a47ea986
robin zhang <lit050528@gmail.com>
2025-06-30 02:26:38 +0800

    Update - add 309

--- Algorithm/go/1_1000/301_400/309/309.go
+++ Algorithm/go/1_1000/301_400/309/309.go
@@ -6,19 +6,27 @@ import (
 )
 
 /*
+思考：
+1、这道题特殊点是有冷冻期，所以需要考虑这个点对于状态转移方程的影响；
+
 思路：动态规划
-状态定义：dp[i][0]表示第i天持有股票的最大收益，dp[i][1]表示第i天不持有股票的最大收益
+状态定义：
+dp[i][0]: 表示第i天持有股票的最大收益
+dp[i][1]: 表示第i天不持有股票的最大收益
 状态转移方程：
-dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i]) // 第i天持有股票，可以是前一天持有，或者前一天不持有并买入
-dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]) // 第i天不持有股票，可以是前一天不持有，或者前一天持有并卖出
+dp[i][0] = max(dp[i-1][0], dp[i-2][1] - prices[i]) // 前一天持有股票，或者前两天不持有股票并且今天买入
+dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i]) // 前一天不持有股票，或者前两天持有股票并且今天卖出
+初始状态：
+dp[0][0] = -prices[0] // 第一天买入股票
+dp[0][1] = 0 // 第一天不持有股票
 */
 func maxProfit(prices []int) int {
 	n := len(prices)
 	dp := make([][2]int, n+2)
-	dp[1][1] = math.MinInt
-	for i, p := range prices {
-		dp[i+2][0] = max(dp[i+1][0], dp[i+1][1]+p) // 持有股票
-		dp[i+2][1] = max(dp[i+1][1], dp[i+1][0]-p) // 不持有股票
+	dp[1][0] = math.MinInt
+	for i, price := range prices {
+		dp[i+2][0] = max(dp[i+1][0], dp[i][1]-price)   // 持有股票
+		dp[i+2][1] = max(dp[i+1][1], dp[i+1][0]+price) // 不持有股票
 	}
 	return dp[n+1][1] // 返回最后一天不持有股票的最大收益
 }
