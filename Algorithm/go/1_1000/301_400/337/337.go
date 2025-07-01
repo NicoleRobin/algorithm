cc0acaffe13a03aa65f7451c555dcb334b2b3af7
robin zhang <lit050528@gmail.com>
2025-07-01 20:04:38 +0800

    Update - add 337

--- Algorithm/go/1_1000/301_400/337/337.go
+++ Algorithm/go/1_1000/301_400/337/337.go
@@ -6,10 +6,19 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+/*
+思路：
+1、
+*/
 func rob(root *TreeNode) int {
 	var ans int
+	if root == nil {
+		return 0
+	}
+	left := rob(root.Left)
+	right := rob(root.Right)
 
-	return ans
+	return max(left+right, root.Val+rob(root.Left.Left)+rob(root.Left.Right)+rob(root.Right.Left)+rob(root.Right.Right))
 }
 
 func main() {
