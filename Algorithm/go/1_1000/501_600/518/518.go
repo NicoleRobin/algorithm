b73202aec12e50ab70a13a0c63abf8deafb119bc
robin zhang <lit050528@gmail.com>
2025-07-04 09:16:45 +0800

    Update - 518

--- Algorithm/go/1_1000/501_600/518/518.go
+++ Algorithm/go/1_1000/501_600/518/518.go
@@ -1,30 +1,49 @@
 package main
 
+import "fmt"
+
 /*
 思考：
 1、首先并没有想到为什么这道题适合动态规划，但是因为是动态规划的主体，所以。。。
 
 思路：动态规划
-状态定义：dp[i][j]表示使用前i种硬币凑成金额j的组合数
-状态转移方程：dp[i][j] = dp[i-1][j] + dp[i][j-coin[i-1]]
-初始状态：dp[0][0] = 1，表示使用0种硬币凑成金额0的组合数为1
-结果：dp[len(coin)][amount]，表示使用所有硬币凑成金额amount的组合数
+状态定义：dp[i]表示金额之和等于x的硬币组合数
+状态转移方程：dp[i] = dp[i-coin]{for coin in coins} + 1
+初始状态：dp[0] = 1，表示凑成金额0的组合数为1
+结果：dp[amount]，表示凑成金额amount的组合数
 */
 func change(amount int, coins []int) int {
-	var ans int
-	n := len(coins)
-	dp := make([][]int, n+1)
-	for i := range dp {
-		dp[i] = make([]int, amount+1)
-	}
-	for i, coin := range coins {
-		for i := 0; i <= amount; i++ {
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if i >= coin {
+				dp[i] = dp[i-coin] + 1
+			}
 		}
 	}
 
-	return dp[n][amount]
+	return dp[amount]
 }
 
 func main() {
+	testCases := []struct {
+		amount int
+		coins  []int
+		want   int
+	}{
+		{
+			amount: 5,
+			coins:  []int{1, 2, 5},
+			want:   4,
+		},
+	}
+	for i, tc := range testCases {
+		ans := change(tc.amount, tc.coins)
+		if ans != tc.want {
+			fmt.Println("test case ", i, " failed, want:", tc.want, " ans:", ans)
+		} else {
+			fmt.Println("test case ", i, " pass")
+		}
+	}
 
 }
