e217250f23b4760d1559a7e4987178f27d298a0f
robin zhang <lit050528@gmail.com>
2025-07-08 22:54:53 +0800

    Update - update 70'

--- Algorithm/go/1_1000/1_100/70/70.go
+++ Algorithm/go/1_1000/1_100/70/70.go
@@ -37,6 +37,20 @@ func rob(nums []int) int {
 	return result
 }
 
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i < n+1; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
 func main() {
 	nums := [][]int{{1, 2, 3, 1}, {2, 7, 9, 3, 1}}
 	for _, num := range nums {
