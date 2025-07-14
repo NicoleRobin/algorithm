4302e88094f6702f187d676431ee7c9f6221b8a2
robin zhang <lit050528@gmail.com>
2025-07-15 06:54:40 +0800

    Update - add 354

--- Algorithm/go/1_1000/301_400/354/354.go
+++ Algorithm/go/1_1000/301_400/354/354.go
@@ -7,6 +7,8 @@ import (
 
 /*
 思路：动态规划
+固定一个维度，然后就可以将题转换为最长递增子序列，同时需要留意<=不同于<，=是不符合要求的；
+
 1、状态定义
 dp[i]: 第i个信封作为结尾的最大信封个数
 2、状态转移方程
@@ -16,30 +18,32 @@ dp[i] = 0到i-1中，信封的宽和高都小于第i个信封的最大值
 
 4、结果
 dp数组的最大值
+
+时间复杂度：n*n
 */
-func maxEnvelopes(envelopes [][]int) int {
+func maxEnvelopes1(envelopes [][]int) int {
+	if len(envelopes) == 0 {
+		return 0
+	}
+
 	sort.Slice(envelopes, func(i, j int) bool {
 		if envelopes[i][0] < envelopes[j][0] {
 			return true
 		} else if envelopes[i][0] == envelopes[j][0] {
-			return envelopes[i][1] < envelopes[j][1]
-		} else {
-			return false
+			return envelopes[i][1] > envelopes[j][1]
 		}
+		return false
 	})
+	// fmt.Println(envelopes)
+
 	n := len(envelopes)
-	dp := make([]int, n+1)
-	dp[0] = 1
+	dp := make([]int, n)
 
 	var ans int
 	for i := 0; i < n; i++ {
-		maxValue := 0
+		var maxValue int
 		for j := 0; j < i; j++ {
-			if envelopes[j][0] >= envelopes[i][0] {
-				break
-			}
-
-			if envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1] {
+			if envelopes[j][1] < envelopes[i][1] {
 				if dp[j] > maxValue {
 					maxValue = dp[j]
 				}
@@ -52,6 +56,33 @@ func maxEnvelopes(envelopes [][]int) int {
 		}
 	}
 	return ans
+}
+
+/*
+思路：二分查找
+
+1、先排序，假设第一个值为宽，第二个值为高；按照宽升序，高降序排序；
+2、然后遍历数组的高，如果当前的高大于已有的所有值，则附加到结尾，否则不做任何处理；
+3、最终记录高的值的数组长度就是结果；
+
+时间复杂度：n*logn
+*/
+func maxEnvelopes(envelopes [][]int) int {
+	sort.Slice(envelopes, func(i, j int) bool {
+		a, b := envelopes[i], envelopes[j]
+		return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+	})
+
+	var f []int
+	for _, e := range envelopes {
+		h := e[1]
+		if i := sort.SearchInts(f, h); i < len(f) {
+			f[i] = h
+		} else {
+			f = append(f, h)
+		}
+	}
+	return len(f)
 }
 
 func main() {
---
+++ Algorithm/go/contest/458/q3/q3.go
@@ -0,0 +1,11 @@
+package main
+
+func processStr(s string) string {
+	var ans []byte
+
+	return string(ans)
+}
+
+func main() {
+
+}
