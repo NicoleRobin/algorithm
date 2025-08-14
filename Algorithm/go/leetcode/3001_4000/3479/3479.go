331807cdf3fb7e9dfb64aa9c33029e0a0e91fb0c
robin zhang <lit050528@gmail.com>
2025-08-15 01:34:32 +0800

    Update - add 3479

--- Algorithm/go/leetcode/3001_4000/3479/3479.go
+++ Algorithm/go/leetcode/3001_4000/3479/3479.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 思路：暴力
 */
@@ -55,21 +57,24 @@ func (st *SegmentTree) build(node, l, r int) {
 }
 
 // Query 区间和查询
-func (st *SegmentTree) Query(qL, qR, max int) int {
-	return st.query(1, 0, st.n-1, qL, qR)
+func (st *SegmentTree) Query(max int) int {
+	return st.query(1, 0, st.n-1, max)
 }
 
-func (st *SegmentTree) query(node, l, r, qL, qR int) int {
-	if qL > r || qR < l {
-		return 0 // 不相交
+func (st *SegmentTree) query(node, l, r, value int) int {
+	if st.tree[node] < value {
+		return -1
 	}
-	if qL <= l && r <= qR {
-		return st.tree[node] // 完全包含
+	if l == r {
+		return l
 	}
+
 	mid := (l + r) / 2
-	left := st.query(node*2, l, mid, qL, qR)
-	right := st.query(node*2+1, mid+1, r, qL, qR)
-	return max(left, right)
+	left := st.query(node*2, l, mid, value)
+	if left != -1 {
+		return left
+	}
+	return st.query(node*2+1, mid+1, r, value)
 }
 
 // Update 更新某个位置的值
@@ -96,12 +101,36 @@ func numOfUnplacedFruits(fruits []int, baskets []int) int {
 
 	var res int
 	for _, fruit := range fruits {
-		if stTree.Query(0, len(baskets)) < fruit {
+		index := stTree.Query(fruit)
+		if index == -1 {
 			res++
+		} else {
+			// update
+			stTree.Update(index, 0)
 		}
 	}
 
 	return res
 }
 
-func main() {}
+func main() {
+	testCases := []struct {
+		fruits   []int
+		baskets  []int
+		expected int
+	}{
+		{
+			fruits:   []int{4, 2, 5},
+			baskets:  []int{3, 5, 4},
+			expected: 1,
+		},
+	}
+	for i, tc := range testCases {
+		res := numOfUnplacedFruits(tc.fruits, tc.baskets)
+		if res != tc.expected {
+			fmt.Printf("Case #%d fail, expected %d, got %d\n", i, tc.expected, res)
+		} else {
+			fmt.Printf("Case #%d pass, expected %d, got %d\n", i, tc.expected, res)
+		}
+	}
+}
