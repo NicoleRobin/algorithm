e4b3b931585b413eb03f55dea80341c414a0820e
robin zhang <lit050528@gmail.com>
2025-08-20 01:04:40 +0800

    Update - add 1277

--- Algorithm/go/leetcode/1001_2000/1277/1277.go
+++ Algorithm/go/leetcode/1001_2000/1277/1277.go
@@ -5,7 +5,28 @@ package main
 dp[i][j]表示以(i,j)为右下角的正方形的边长
 */
 func countSquares(matrix [][]int) int {
+	if len(matrix) == 0 {
+		return 0
+	}
 
+	m := len(matrix)
+	n := len(matrix[0])
+	dp := make([][]int, m+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, n+1)
+	}
+
+	var ans int
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 1 {
+				dp[i+1][j+1] = min(min(dp[i+1][j], dp[i][j+1]), dp[i][j]) + 1
+				ans += dp[i+1][j+1]
+			}
+		}
+	}
+
+	return ans
 }
 
 func main() {
---
+++ Algorithm/go/leetcode/2001_3000/2348/2348.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+func zeroFilledSubarray(nums []int) int64 {
+	if len(nums) == 0 {
+
+	}
+
+	var subArrayLenList []int
+	var res int64
+	var zeroCount int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			zeroCount++
+		} else {
+			subArrayLenList = append(subArrayLenList, zeroCount)
+			zeroCount = 0
+		}
+	}
+	if zeroCount > 0 {
+		subArrayLenList = append(subArrayLenList, zeroCount)
+	}
+
+	fmt.Println(subArrayLenList)
+	for _, count := range subArrayLenList {
+		res += int64((1 + count) * count / 2)
+	}
+
+	return res
+}
+
+func main() {
+	testCases := []struct {
+		nums     []int
+		expected int64
+	}{
+		{
+			nums:     []int{1, 3, 0, 0, 2, 0, 0, 4},
+			expected: 6,
+		},
+		{
+			nums:     []int{0, 0, 0, 2, 0, 0},
+			expected: 9,
+		},
+	}
+	for i, tc := range testCases {
+		res := zeroFilledSubarray(tc.nums)
+		if res != tc.expected {
+			fmt.Printf("case #%d fail: got %d, expected %d\n", i+1, res, tc.expected)
+		} else {
+			fmt.Printf("case #%d success: got %d, expected %d\n", i+1, res, tc.expected)
+		}
+	}
+}
