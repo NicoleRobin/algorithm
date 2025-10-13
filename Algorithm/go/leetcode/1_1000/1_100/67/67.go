f4978c2360e951ee424604513712707a05c4f135
robin zhang <lit050528@gmail.com>
2025-10-14 07:11:55 +0800

    Update - add 67

--- Algorithm/go/leetcode/1_1000/1_100/67/67.go
+++ Algorithm/go/leetcode/1_1000/1_100/67/67.go
@@ -1,31 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 /*
 思考：
 1、低位在右边，需要从右边开始计算
-2、
+2、用一个数字分别记录某一位的和，分别加上a/b以及carry
 */
 func addBinary(a string, b string) string {
+	ans := ""
+	carry := 0
+
+	i, j := len(a)-1, len(b)-1
+	for i >= 0 || j >= 0 || carry > 0 {
+		sum := carry
+		if i >= 0 {
+			sum += int(a[i] - '0')
+			i--
+		}
+		if j >= 0 {
+			sum += int(b[j] - '0')
+			j--
+		}
+
+		ans = strconv.Itoa(sum%2) + ans
+		carry = sum / 2
+	}
+	return ans
 }
 
 func main() {
 	testCases := []struct {
 		a, b   string
-		expect int
+		expect string
 	}{
 		{
-			a:      "",
-			b:      "",
-			expect: -2,
+			a:      "11",
+			b:      "1",
+			expect: "100",
 		},
 	}
 
 	for i, testCase := range testCases {
 		ans := addBinary(testCase.a, testCase.b)
 		if ans != testCase.expect {
-			fmt.Printf("case %d fail, a:%d, b:%d, expect:%d, ans:%d\n", i, testCase.a, testCase.b, testCase.expect, ans)
+			fmt.Printf("case %d fail, a:%s, b:%s, expect:%s, ans:%s\n", i, testCase.a, testCase.b, testCase.expect, ans)
 		} else {
 			fmt.Printf("case %d pass\n", i)
 		}
