e393618d31b83638b8928ba9cfa02f11bcefdccc
robin zhang <lit050528@gmail.com>
2025-11-24 23:30:10 +0800

    Update - add lcr 5

--- Algorithm/go/leetcode/lcr/5/5.go
+++ Algorithm/go/leetcode/lcr/5/5.go
@@ -4,7 +4,34 @@ import "fmt"
 
 func maxProduct(words []string) int {
 	var ans int
+	for i := 0; i < len(words); i++ {
+		for j := i + 1; j < len(words); j++ {
+			duplicate := isDuplicate(words[i], words[j])
+			if duplicate {
+				continue
+			}
 
+			if ans < len(words[i])*len(words[j]) {
+				ans = len(words[i]) * len(words[j])
+			}
+		}
+	}
+
+	return ans
+}
+
+func isDuplicate(a, b string) bool {
+	var ans bool
+	charAMap := map[byte]bool{}
+	for i := 0; i < len(a); i++ {
+		charAMap[a[i]] = true
+	}
+	for i := 0; i < len(b); i++ {
+		if _, ok := charAMap[b[i]]; ok {
+			ans = true
+			break
+		}
+	}
 	return ans
 }
 
