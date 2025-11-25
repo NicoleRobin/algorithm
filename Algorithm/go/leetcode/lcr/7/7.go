567868e1a6534a94ef40e97ea4b644af3991155c
robin zhang <lit050528@gmail.com>
2025-11-26 00:49:03 +0800

    Update - add lcr 7

--- Algorithm/go/leetcode/lcr/7/7.go
+++ Algorithm/go/leetcode/lcr/7/7.go
@@ -1,9 +1,65 @@
 package main
 
-func threeSum(nums []int) [][]int {
-	var ans [][]int
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
 
+/*
+和LeetCode15是相同的；
+需要考虑去重，
+*/
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	fmt.Println(nums)
+
+	var ans [][]int
+	for i := 0; i < len(nums); i++ {
+		twoSumAns := twoSum(nums[i+1:], nums[i]*-1)
+		if len(twoSumAns) > 0 {
+			twoSumAns = append(twoSumAns, nums[i])
+			ans = append(ans, twoSumAns)
+		}
+	}
 	return ans
 }
 
-func main() {}
+func twoSum(nums []int, target int) []int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		tmpSum := nums[left] + nums[right]
+		if tmpSum == target {
+			return []int{nums[left], nums[right]}
+		}
+		if tmpSum < target {
+			left++
+		}
+		if tmpSum > target {
+			right--
+		}
+	}
+	return nil
+}
+
+func main() {
+	testCases := []struct {
+		nums     []int
+		target   int
+		expected [][]int
+	}{
+		{
+			nums:     []int{-1, 0, 1, 2, -1, -4},
+			target:   1,
+			expected: [][]int{{-1, -1, 2}, {-1, 0, 1}},
+		},
+	}
+	for _, testCase := range testCases {
+		result := threeSum(testCase.nums)
+		if !reflect.DeepEqual(result, testCase.expected) {
+			fmt.Printf("%d expected %d, got %d\n", testCase.nums, testCase.expected, result)
+		} else {
+			fmt.Printf("%d got %d\n", testCase.nums, testCase.expected)
+		}
+	}
+}
